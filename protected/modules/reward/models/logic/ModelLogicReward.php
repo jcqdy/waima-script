6ce1d8dd6caa23378f235d90e4a05e0c847f861c
jcqdy <jcqdy@sina.com>
2017-02-12 19:42:26 +0800

    no message

--- protected/modules/reward/models/logic/ModelLogicReward.php
+++ protected/modules/reward/models/logic/ModelLogicReward.php
@@ -95,7 +95,7 @@ class ModelLogicReward
 
         $mock = false;
         // 如果最近一次用户分享距离现在大于2小时
-        if (isset($data[1]) && $time - $data[1]['createTime'] > 7200) {
+        if ((isset($data[1]) && $time - $data[1]['createTime'] > 7200) || empty($data)) {
             $data = $this->modelDaoReward->randByUserTag(2);
             $mock = true;
         }
