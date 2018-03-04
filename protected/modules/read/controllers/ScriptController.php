1ebdadec081378203eb38a6fac0961e2673e90fe
jcqdy <jcqdy@sina.com>
2018-03-04 22:37:06 +0800

    no message

--- protected/modules/read/controllers/ScriptController.php
+++ protected/modules/read/controllers/ScriptController.php
@@ -17,8 +17,8 @@ class ScriptController extends Controller
         $userId = ParameterValidatorHelper::validateMongoIdAsString($_POST, 'userId');
         $scriptId = ParameterValidatorHelper::validateMongoIdAsString($_POST, 'scriptId');
         $readPos = ParameterValidatorHelper::validateInteger($_POST, 'readPos');
-        $fontSize = ParameterValidatorHelper::validateEnumInteger($_POST, 'readPos', [13,15,17,19,21]);
-        $backColor = ParameterValidatorHelper::validateEnumInteger($_POST, 'readPos', [1,2,3,4]);
+        $fontSize = ParameterValidatorHelper::validateEnumInteger($_POST, 'fontSize', [13,15,17,19,21]);
+        $backColor = ParameterValidatorHelper::validateEnumInteger($_POST, 'backColor', [1,2,3,4]);
 
         $modelLogicPutStatus = new ModelLogicPutStatus();
         $modelLogicPutStatus->execute($userId, $scriptId, $readPos, $fontSize, $backColor);
