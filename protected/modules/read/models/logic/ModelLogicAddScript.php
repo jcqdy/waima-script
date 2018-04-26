6a5adcfc613d607918e4f5972fcaa162740f2ba1
jcqdy <jcqdy@sina.com>
2018-04-26 15:02:47 +0800

    no message

--- protected/modules/read/models/logic/ModelLogicAddScript.php
+++ protected/modules/read/models/logic/ModelLogicAddScript.php
@@ -11,23 +11,23 @@ class ModelLogicAddScript
     public function execute($userId, $scriptId)
     {
         $bookCase = $this->modelDataBookCase->getBookCase($userId);
-
-        $bookCase = empty($bookCase) ? [] : $bookCase['scriptIds'];
-
         if (empty($bookCase)) {
-            $ret = $this->modelDataBookCase->addScript($userId, [$scriptId]);
-        } else {
-            foreach ($bookCase as $val) {
-                if (is_string($val) && $val == $scriptId)
-                    return [];
-
-                if (is_array($val) && in_array($scriptId, $val['scriptIds']))
-                    return [];
-            }
-
-            array_unshift($bookCase, $scriptId);
-            $ret = $this->modelDataBookCase->modifyScriptIds($userId, $bookCase);
+            $this->modelDataBookCase->addScript($userId, [$scriptId]);
+            return [];
         }
+
+        $bookCase = $bookCase['scriptIds'];
+
+        foreach ($bookCase as $val) {
+            if (is_string($val) && $val == $scriptId)
+                return [];
+
+            if (is_array($val) && in_array($scriptId, $val['scriptIds']))
+                return [];
+        }
+
+        array_unshift($bookCase, $scriptId);
+        $ret = $this->modelDataBookCase->modifyScriptIds($userId, $bookCase);
 
         if ($ret === false)
             throw new Exception('add script failed', Errno::FATAL);
